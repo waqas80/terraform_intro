e35ee5fa4450b895008be958b2b0bcfd145d5a63
Waqas <waqas80@msn.com>
2019-11-10 23:43:55 +0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+    default = "us-east-1"
+}
+variable "aws_access_key" {
+    default = "XXXXXXXXXXXXXXXXXXX"
+}
+
+variable "aws_secret_key" {
+    default = "XXXXXXXXXXXXXXXXXXXXXXXXX"
+}
